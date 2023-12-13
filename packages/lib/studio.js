9fc53a8076a6689705bae55be039421e937a9f85
Joel Wetzell <jwetzell@yahoo.com>
2023-12-13 14:15:00 -0600

    add socket connection and eventemitter to studio

--- packages/lib/studio.js
+++ packages/lib/studio.js
@@ -1,4 +1,13 @@
-class Studio {
+const { EventEmitter } = require('node:events');
+const { Socket } = require('node:net');
+
+class Studio extends EventEmitter {
+  constructor(ip) {
+    super();
+    this.ip = ip;
+    this.connect();
+  }
+
   update(packet) {
     const packetParts = packet.trim().split(':');
 
@@ -12,6 +21,52 @@ class Studio {
       default:
         console.error(`lib: unrecognized packet type: ${this.latestPacket.type}`);
         break;
+    }
+
+    this.emit('update');
+  }
+
+  #setupSocket() {
+    if (this.socket) {
+      this.socket.destroy();
+      this.socket.removeAllListeners();
+    }
+
+    this.socket = new Socket();
+    this.socket.on('connect', () => {
+      this.connected = true;
+      console.log('studio: successfully connected');
+      this.emit('connect');
+    });
+
+    this.socket.on('error', (error) => {
+      this.connected = false;
+      console.error(`studio: socket error -> ${error.message}`);
+      setTimeout(() => {
+        this.connect();
+      }, 5000);
+      this.emit('error', error);
+    });
+
+    this.socket.on('data', (data) => {
+      console.log(data);
+      this.update(data.toString());
+    });
+
+    this.socket.on('close', (hadError) => {
+      this.connected = false;
+      console.log(`studio: connection closed ${hadError ? 'with error' : 'normally'}`);
+      setTimeout(() => {
+        this.connect();
+      }, 5000);
+      this.emit('close', hadError);
+    });
+  }
+
+  connect() {
+    if (!this.connected && this.ip) {
+      this.#setupSocket();
+      this.socket.connect(9923, this.ip);
     }
   }
 }
