2e9748bc959a4e565398a50b5fb864f50853fd04
Joel Wetzell <jwetzell@yahoo.com>
2023-12-13 13:31:32 -0600

    add switch for incoming packet types

--- packages/lib/studio.js
+++ packages/lib/studio.js
@@ -1,11 +1,19 @@
 class Studio {
+  update(packet) {
+    const packetParts = packet.trim().split(':');
 
-    update(packet){
-        this.latestPacket = {
-            parts: packet.trim().split(':'),
-            raw: packet
-        }
+    this.latestPacket = {
+      type: packetParts[0],
+      parts: packetParts,
+      raw: packet,
+    };
+
+    switch (this.latestPacket.type) {
+      default:
+        console.error(`lib: unrecognized packet type: ${this.latestPacket.type}`);
+        break;
     }
+  }
 }
 
 module.exports = Studio;
