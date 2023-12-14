7b63f7095870b53e72dd65808af83e4cd6a194d7
Joel Wetzell <jwetzell@yahoo.com>
2023-12-14 12:36:20 -0600

    add input type constants

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Studio = require('./src/studio');
+const CONSTANTS = require('./src/constants');
 
 module.exports = {
   Studio,
+  CONSTANTS,
 };
---
+++ src/constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+  INPUT_TYPES: {
+    1: 'CAMERA',
+    2: 'GFX',
+    3: 'MEDIA',
+    4: 'REMOTE',
+    5: 'COLOR',
+  },
+};
