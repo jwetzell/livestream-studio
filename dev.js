d2f8f97a3f738cb1fa46f116a9e694fbba7604bc
Joel Wetzell <jwetzell@yahoo.com>
2023-12-15 13:25:47 -0600

    update dev script

--- dev.js
+++ dev.js
@@ -1,3 +1,18 @@
 const { Studio } = require('.');
 
-const studio = new Studio();
+const studio = new Studio('127.0.0.1'); // IP of Livestream Studio
+
+studio.on('connect', () => {
+  console.log('studio has connected');
+});
+
+studio.on('update', (type) => {
+  // type is the type of packet that caused the update
+  console.log('studio update received');
+  console.log(studio.toJSON());
+});
+
+// Send the cut command to the studio instance every 10 seconds
+setInterval(() => {
+  studio.cut();
+}, 10000);
